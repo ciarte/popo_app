9adf91f5924f33f9cebc0595cd78f8fc23c9a859
Gabriel Iciarte <gabriel_viki@hotmail.com>
2026-01-19 18:10:08 -0300

    g

--- lib/page_0ne.dart
+++ lib/page_0ne.dart
@@ -11,31 +11,42 @@ class PageOne extends StatelessWidget {
         child: Icon(Icons.reset_tv),
         onPressed: () => Navigator.pushNamed(context, 'page_two'),
       ),
-      body: SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Usuario',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Divider(),
-              Text('nombre:'),
-              Text('edad:'),
-              SizedBox(height: 10),
-              Divider(),
-              Text(
-                'Profesiones',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Text('Profesion 1:'),
-              Text('Profesion 1:'),
-            ],
-          ),
+      body: InitialInfoWidget(),
+    );
+  }
+}
+
+class InitialInfoWidget extends StatelessWidget {
+  const InitialInfoWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Usuario',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Divider(),
+            Text('nombre:'),
+            Text('edad:'),
+            SizedBox(height: 10),
+            Divider(),
+            Text(
+              'Profesiones',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Text('Profesion 1:'),
+            Text('Profesion 1:'),
+          ],
         ),
       ),
     );
